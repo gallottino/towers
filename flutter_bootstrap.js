8faaa9eb9571e1c28b450d7a8dc280ecc7f52aef
gallottino <d.gallotti@outlook.it>
2025-04-27 18:45:35 +0200

    deploy: new build

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "329984664"
+    serviceWorkerVersion: "2886535038"
   }
 });
